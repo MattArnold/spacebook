97f9c3ce0779c70697d3a576b2053dbad45bda56
Matt Arnold <matt.mattarn@gmail.com>
2024-08-17 15:49:49 -0400

    Debug Netlify build

--- .eleventy.js
+++ .eleventy.js
@@ -224,23 +224,23 @@ module.exports = function(eleventyConfig) {
         tokens[idx].attrPush([ 'rel', 'noopener noreferrer' ])
       }
     })
-    .use(markdownItAnchor, opts)
-    .use(markdownItEmoji)
-    .use(markdownItFootnote)
-    .use(markdownItContainer, 'callout')
-    .use(markdownItContainer, 'callout-blue')
-    .use(markdownItContainer, 'callout-pink')
-    .use(markdownItContainer, 'callout-green')
-    .use(markdownItContainer, 'warning')
-    .use(markdownItTasks)
-    .use(markdownItCenterText)
-    .use(markdownLinkifyImages, {
-      imgClass: "p-4",
-    })
-    .use(markdownItAttrs, {
-      includeLevel: [2,3],
-      listType: "ol"
-    })
+    // .use(markdownItAnchor, opts)
+    // .use(markdownItEmoji)
+    // .use(markdownItFootnote)
+    // .use(markdownItContainer, 'callout')
+    // .use(markdownItContainer, 'callout-blue')
+    // .use(markdownItContainer, 'callout-pink')
+    // .use(markdownItContainer, 'callout-green')
+    // .use(markdownItContainer, 'warning')
+    // .use(markdownItTasks)
+    // .use(markdownItCenterText)
+    // .use(markdownLinkifyImages, {
+    //   imgClass: "p-4",
+    // })
+    // .use(markdownItAttrs, {
+    //   includeLevel: [2, 3],
+    //   listType: "ol"
+    // })
   );
 
   return {
